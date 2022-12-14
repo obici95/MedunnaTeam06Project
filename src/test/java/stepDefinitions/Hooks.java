462ac59e9effe913808e9f2f54c7ca4572dd840e
Abdullah AVLIK <abdullah.avlik@gmail.com>
2022-12-14 17:32:08 +0300

    2022/22/14

--- src/test/java/stepDefinitions/Hooks.java
+++ src/test/java/stepDefinitions/Hooks.java
@@ -18,6 +18,6 @@ public class Hooks {
         if (scenario.isFailed()) {
             scenario.attach(screenshot, "image/png","screenshots");
         }
-        Driver.quitDriver();
+        //Driver.quitDriver();
     }
 }
